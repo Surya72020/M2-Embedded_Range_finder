3b8e3ce7a61b329f3c4ae4b4f03310466a62498a
Surya72020 <94237507+Surya72020@users.noreply.github.com>
2021-12-02 13:49:59 +0530

    Update LCD_16x2_H_file.h

--- 3_Implementation/inc/LCD_16x2_H_file.h
+++ 3_Implementation/inc/LCD_16x2_H_file.h
@@ -10,13 +10,13 @@
 #include <avr/io.h>						/* Include AVR std. library file */
 #include <util/delay.h>					/* Include inbuilt defined Delay header file */
 
-#define LCD_Data_Dir DDRB				/* Define LCD data port direction */
+#define LCD_Data_Dir DDRD				/* Define LCD data port direction */
 #define LCD_Command_Dir DDRC			/* Define LCD command port direction register */
-#define LCD_Data_Port PORTB				/* Define LCD data port */
+#define LCD_Data_Port PORTD				/* Define LCD data port */
 #define LCD_Command_Port PORTC			/* Define LCD data port */
-#define EN PC2							/* Define Enable signal pin */
-#define RW PC3							/* Define Read/Write signal pin */
-#define RS PC4							/* Define Register Select (data reg./command reg.) signal pin */
+#define EN PC3							/* Define Enable signal pin */
+#define RW PC4							/* Define Read/Write signal pin */
+#define RS PC5							/* Define Register Select (data reg./command reg.) signal pin */
 
 void LCD_Command (char);				/* LCD command write function */
 void LCD_Char (char);					/* LCD data write function */
